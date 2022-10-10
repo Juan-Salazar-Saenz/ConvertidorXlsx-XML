21dbd201a5cba83543a6663ee212005169e20e88
Juan-Salazar-Saenz <tec.juan.0927cacd@gmail.com>
2022-10-10 11:03:47 -0500

    Creacion archivo XML

--- index.py
+++ index.py
@@ -5,7 +5,9 @@ from tkinter import filedialog       #Codigo para importar desde windows en vent
 import tkinter
 import openpyxl                      #Codigo para leer archivos excel
 from io import open                  #Codigo para escribir archivo en txt
+import xml.etree.ElementTree as cxml #Codigo para escribir archivo en XML
 
+    
 #configuracion de la venta
 root = Tk()
 root.geometry("650x300")
@@ -45,7 +47,8 @@ tkinter.Label(root, text="Cantidad de registros").place(x = 10, y = 100)
 
 tkinter.Label(root, state=DISABLED, textvariable=registros).place(x = 130, y = 100) 
 
-def convertir():    
+def convertir():  
+    
     try:
         #Se paramos el archivo en carpetas
         ruta = ubicacion.get().split(sep="/")
@@ -62,10 +65,9 @@ def convertir():
             hojadeExcel = archivoExcel[nombreHoja.get()]  #Seleccionamos la hoja de excel
             registros.set(len(hojadeExcel['A'])-1)        #Contamos el numero de lineas
             archivoXML = escribirXML()                    #Creamos el archivo de texto .XML
-            
+            archivoXML.write('<?xml version="1.0" encoding="UTF-8"?> \n')
             for fila in hojadeExcel:
-                datos = [celda.value for celda in fila]
-                print(datos)
+                datos = [celda.value for celda in fila]                
                 textoXML = "" 
                 for i in range(len(datos)):   
                     if datos[i] == None:
@@ -73,8 +75,10 @@ def convertir():
                     else:
                         textoXML += str(datos[i]) + ";"                    
                 
-                archivoXML.write(textoXML + " \n")        #Escribimos archivo            
-            
+                #archivoXML.write(textoXML + " \n")        #Escribimos archivo            
+
+            textoXML = construccionXML()  
+            archivoXML.write(textoXML)
             archivoExcel.close                            #Cerramos el archivo
         except:
             registros.set("Hoja inexistente,¡ intente de nuevo !")        
@@ -103,6 +107,72 @@ def escribirXML():
         return archivoTexto
     except:
         salida.set("Archivo existente,¡ intente de nuevo !")
+
+def construccionXML():
+    xml = cxml.Element('garantias')
+
+    op = cxml.SubElement(xml, "op")
+
+    cxml.SubElement(op, "t").text="I"
+    cxml.SubElement(op, "tg").text="1"
+    cxml.SubElement(op, "hash").text="3925"
+
+    glc = cxml.SubElement(xml, "glc")
+    ddor = cxml.SubElement(glc, "ddor")
+    
+    cxml.SubElement(ddor, "cci").text="1"
+    cxml.SubElement(ddor, "ni").text="5022743"
+    cxml.SubElement(ddor, "pn").text="ABELARDO"
+    cxml.SubElement(ddor, "pa").text="BARRETO"
+    cxml.SubElement(ddor, "sa").text="GUILLEN"
+    cxml.SubElement(ddor, "pais").text="CO"
+    cxml.SubElement(ddor, "dpto").text="8"    
+    cxml.SubElement(ddor, "mun").text="08001"
+    cxml.SubElement(ddor, "dir").text="CR 9 A 17 19"
+    cxml.SubElement(ddor, "email").text="abelardobarretoguillen@hotmail.com"
+    cxml.SubElement(ddor, "tel").text="3719150"
+    cxml.SubElement(ddor, "cel").text="3114097178"
+    cxml.SubElement(ddor, "tdc").text="0"
+    cxml.SubElement(ddor, "ins").text="false"
+    cxml.SubElement(ddor, "gen").text="1"
+    cxml.SubElement(ddor, "tddor").text="d"
+
+    acdor = cxml.SubElement(glc, "acdor")
+
+    cxml.SubElement(acdor, "cci").text="6"
+    cxml.SubElement(acdor, "ni").text="860003020"
+    cxml.SubElement(acdor, "dv").text="1"
+    cxml.SubElement(acdor, "rs").text="BANCO BILBAO VIZCAYA ARGENTARIA COLOMBIA S.A.BBVA COLOMBIA"
+    cxml.SubElement(acdor, "pais").text="CO"
+    cxml.SubElement(acdor, "dpto").text="11"
+    cxml.SubElement(acdor, "mun").text="11001"
+    cxml.SubElement(acdor, "dir").text="KR 9 72-21 PI '7'"
+    cxml.SubElement(acdor, "email").text="Garantiasmobiliarias@confecamaras.org.co"
+    cxml.SubElement(acdor, "tel").text="3471600"
+    cxml.SubElement(acdor, "cel").text="0313471600"
+    cxml.SubElement(acdor, "ppal").text="true"
+    cxml.SubElement(acdor, "ppar").text="100"
+
+    descbien = cxml.SubElement(glc, "descbien").text="Vehiculo"
+    prad = cxml.SubElement(glc, "prad").text="true"
+    bienes = cxml.SubElement(glc, "bienes")
+
+    cxml.SubElement(bienes, "ctb").text="1"
+    cxml.SubElement(bienes, "marca").text="KIA"
+    cxml.SubElement(bienes, "srial").text="KNABE511AHT359063"
+    cxml.SubElement(bienes, "mdlo").text="2017"
+    cxml.SubElement(bienes, "placa").text="WGD259"
+    cxml.SubElement(bienes, "desc").text="Vehiculo"
+    cxml.SubElement(bienes, "fabric").text="GM Colmotores"
+
+    monto = cxml.SubElement(glc, "monto").text="46350000"
+    vdef = cxml.SubElement(glc, "vdef").text="1"
+    ffin = cxml.SubElement(glc, "ffin").text="2032-12-30T23:59:59.0000000"
+    ctg = cxml.SubElement(glc, "ctg").text="1"
+    cm = cxml.SubElement(glc, "cm").text="COP"
+
+    cxml.indent(xml)                                  #identamos el XML creado
+    return cxml.tostring(xml, encoding='unicode')     #Imprimimos el XML
 
 tkinter.Button (root, text=" * Convertir * ", command=convertir).place(x = 100, y = 130) 
 
